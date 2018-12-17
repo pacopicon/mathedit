d43a026369cb3a667ecd986f016f6c13c797aa14
pacopicon <palmtreerooskee@gmail.com>
2018-12-17 10:21:21 -0500

    wip++

--- FE/src/MathPad.js
+++ FE/src/MathPad.js
@@ -140,114 +140,210 @@ class MathPad extends Component {
   //     return all
     
 
-  //   // for (let i=0; i<children.length; i++) {
-  //   //   let child = children[i]
-  //   //   if (child.tagName == VAR) {
-  //   //     latex += `${child.innerText}`
-  //   //   } else if (child.tagName == SPAN) {
-  //   //     if (typeof child.className == 'undefined' && child.innerText) {
-  //   //       latex += `${child.innerText}`
-  //   //     } else if (child.className == SUBSCRIPT || child.className == SUPERSCRIPT) {
-  //   //       if (child.className == SUBSCRIPT) {
-  //   //         latex += `_${child.innerText}`
-  //   //       } else if (child.className == SUPERSCRIPT) {
-  //   //         latex += `^${child.innerText}`
-  //   //       }
-  //   //       if (child.children.length > 0) {
-  //   //         let mq_sup = child.children[0]
-  //   //         let mq_sup_children = mq_sup.children
-  //   //         if (mq_sup_children) {
-  //   //           this.parseMathDOM(mq_sup_children, latex)
-  //   //         }
-  //   //       }
-  //   //     } else if (child.className == BINARY_OP) {
-  //   //       latex += `${child.innerText}`
-  //   //     } else if (child.className == PARENTHESES) {
-  //   //       if (child.innerText == '(') {
-  //   //         latex += `\\left${child.innerText}`
-  //   //       } else {
-  //   //         latex += `\\right${child.innerText}`
-  //   //       }
-  //   //     } else if (child.className == FRACTION) {
-  //   //       if (child.children.length > 0) {
-  //   //         let grand_children = child.children
-  //   //         if (grand_children.className == NUMERATOR) {
-  //   //           latex += `\\frac{`
-  //   //           this.this.parseMathDOM(grand_children.children, latex)
-  //   //           latex += `}`
-  //   //         } else if (grand_children.className == DENOMINATOR) {
-  //   //           latex += `{`
-  //   //           this.parseMathDOM(grand_children.children, latex)
-  //   //           latex += `}`
-  //   //         }
-  //   //       }
-  //   //     } else if (child.className == GROUP) {
-  //   //       if (child.children.length > 0) {
-  //   //         this.parseMathDOM(child.children, latex)
-  //   //       }
-  //   //     }
-  //   //   }
-  //   // }
-  //   // console.log('latex = ', latex)
-  //   // return latex
+    // for (let i=0; i<children.length; i++) {
+    //   let child = children[i]
+    //   if (child.tagName == VAR) {
+    //     latex += `${child.innerText}`
+    //   } else if (child.tagName == SPAN) {
+    //     if (typeof child.className == 'undefined' && child.innerText) {
+    //       latex += `${child.innerText}`
+    //     } else if (child.className == SUBSCRIPT || child.className == SUPERSCRIPT) {
+    //       if (child.className == SUBSCRIPT) {
+    //         latex += `_${child.innerText}`
+    //       } else if (child.className == SUPERSCRIPT) {
+    //         latex += `^${child.innerText}`
+    //       }
+    //       if (child.children.length > 0) {
+    //         let mq_sup = child.children[0]
+    //         let mq_sup_children = mq_sup.children
+    //         if (mq_sup_children) {
+    //           this.parseMathDOM(mq_sup_children, latex)
+    //         }
+    //       }
+    //     } else if (child.className == BINARY_OP) {
+    //       latex += `${child.innerText}`
+    //     } else if (child.className == PARENTHESES) {
+    //       if (child.innerText == '(') {
+    //         latex += `\\left${child.innerText}`
+    //       } else {
+    //         latex += `\\right${child.innerText}`
+    //       }
+    //     } else if (child.className == FRACTION) {
+    //       if (child.children.length > 0) {
+    //         let grand_children = child.children
+    //         if (grand_children.className == NUMERATOR) {
+    //           latex += `\\frac{`
+    //           this.this.parseMathDOM(grand_children.children, latex)
+    //           latex += `}`
+    //         } else if (grand_children.className == DENOMINATOR) {
+    //           latex += `{`
+    //           this.parseMathDOM(grand_children.children, latex)
+    //           latex += `}`
+    //         }
+    //       }
+    //     } else if (child.className == GROUP) {
+    //       if (child.children.length > 0) {
+    //         this.parseMathDOM(child.children, latex)
+    //       }
+    //     }
+    //   }
+    // }
+    // console.log('latex = ', latex)
+    // return latex
   // }
   
   
   getCursorAdjacentString() {
 
     let rootNode = (document.getElementsByClassName('mq-root-block'))[0]
-    let rootNodeChildren = rootNode.children
-    console.log('rootNodeChildren = ', rootNodeChildren)
+    
+    const VAR = 'VAR'
+    const SPAN = 'SPAN'
+    const SUBSCRIPT = 'mq-supsub mq-non-leaf'
+    const SUPERSCRIPT =  'mq-supsub mq-non-leaf mq-sup-only'
+    const BINARY_OP = 'mq-binary-operator'
+    const PARENTHESES = 'mq-scaled mq-paren'
+    const FRACTION = 'mq-fraction mq-non-leaf'
+    const NUMERATOR = 'mq-numerator'
+    const DENOMINATOR = 'mq-denominator'
+    const GROUP = 'mq-non-leaf'
+    const SUB = 'mq-sub'
+    const SUP = 'mq-sup'
 
-    // let cursor = document.getElementsByClassName('mq-cursor')
-    // console.log('cursor = ', cursor)
-    
-    // let hasCursor = document.querySelector('.mq-hasCursor')
-    // console.log('hasCursor = ', hasCursor)
-    
-    let cursorParent = document.querySelector('.mq-cursor').parentNode
-    console.log('cursorParent = ', cursorParent)
+    const returnCursorParentTree = (child, _latex) =>{
+      let latex = _latex ? _latex : ''
+      // let output = typeof _output != 'undefined' ? [..._output] : [child]
+
+      if (child && child.parentNode && child.parentNode.children) {
+        let parent = child.parentNode
+        let siblingsObj = child.parentNode.children
+        let siblings = []
+
+        const isSeries = (arr) => {
+          arr.sort()
+          let isSeries = true
+          for (let i = 0; i<arr.length-1; i++) {
+            // console.log(`first = ${arr[i]}, second = ${i+1}...${arr[i+1] - arr[i]}`)
+            if (arr[i+1] - arr[i] != 1) {
+              isSeries = false
+            }
+          }
+          return isSeries
+        }
+        
+        const getObjNumPropLen = (obj) => {
+          let arr = []
+          let output = null
+          if (!Array.isArray(obj)) {
+            for (let p in obj) {
+              let num = Number(p)
+              if (Number.isInteger(num)) {
+                arr.push(num)
+              }
+            }
+          } else {
+            arr = obj
+          }
+          // verify
+          if (isSeries(arr)) {
+            return arr.length
+          } 
+          return output
+        }
+        const getChildren = (siblingsObj, _latex) => {
+          let latex = _latex ? _latex : ''
+          let len = getObjNumPropLen(siblingsObj)
+          console.log('len = ', len)
+          for (let i=0; i<len; i++) {
+            let p = `${i}`
+            let sib = siblingsObj[p]
+            // console.log('sib = ', sib)
+            if (sib.className == SUB || sib.className == SUP) {
+              if (sib.children.length > 0) {
+                console.log('sib.children.length = ', sib.children.length)
+                console.log('sib.children = ', sib.children)
+                console.log('sib.children[0] = ', sib.children[0])
+               getChildren(sib, latex) 
+              }
+            } else if (sib.innerText) {
+              
+              latex += `${sib.innerText}`
+            }
+          }
+        }
+        
+        let sibLen = getObjNumPropLen(siblingsObj)
+        // console.log('siblingsObj.length = ', siblingsObj.length)
+
+        for (let i=0; i<sibLen; i++) {
+          let p = `${i}`
+      
+          if (siblingsObj[p].tagName || siblingsObj[p].className) {
+            let tn = siblingsObj[p].tagName
+            let cn = siblingsObj[p].className
+            // console.log(`${tn}.${cn}.typeof cn= ${typeof cn}`)
+            let sib = siblingsObj[p]
+            if (sib.tagName == VAR) {
+              latex += `${sib.innerText}`
+              // console.log('sib.innerText = ', sib.innerText)
+              siblings.push(sib.innerText)
+            } else if (sib.tagName == SPAN) {
+              if (sib.className == '' && !sib.attributes['style']) {
+                latex += `${sib.innerText}`
+                // console.log('sib = ', sib)
+                // console.log('sib.innerText = ', sib.innerText)
+                siblings.push(sib.innerText)
+              } else if (sib.className == SUB || sib.className == SUP) {
+                if (sib.children.length > 0) {
+                  // console.log('sib.children.length = ', sib.children.length)
+                  // console.log('sib.children = ', sib.children)
+                  // console.log('sib.children[0] = ', sib.children[0])
+                //  returnCursorParentTree(sib, latex) 
+                }
+              } else if (sib.className == SUBSCRIPT || sib.className == SUPERSCRIPT) {
+                if (sib.className == SUBSCRIPT) {
+                  latex += `_${sib.innerText}`
+                } else if (sib.className == SUPERSCRIPT) {
+                  latex += `^${sib.innerText}`
+                }
+                if (sib.children.length > 0) {
+                  let mq_sup = sib.children[0]
+                  let mq_sup_children = mq_sup.children
+                  // if (mq_sup_children) {
+                    // console.log('sib.children.length = ', sib.children.length)
+                    // console.log('sib.children = ', sib.children)
+                    // console.log('sib.children[0] = ', sib.children[0])
+                    // console.log('Array.isArray(sib.children[0]) = ', Array.isArray(sib.children[0]))
+                    // returnCursorParentTree(sib, latex)
+                    getChildren(sib.children, latex)
+                  // }
+                }
+              } 
+            } else {
+              siblings.push(siblingsObj[p])
+            }
+            // console.log(`${tn}.${cn}`)
+          }
+          
+          
+        }
+        // output.push(siblings)
   
-    let cursorGrandParent = cursorParent.parentNode
-    console.log('cursorGrandParent = ', cursorGrandParent)
-
-
-    // let curParChildren = hasCursor.children
-    // console.log('curParChildren = ', curParChildren)
-
-    // let nextVar = document.querySelector('.mq-cursor + var')
-    // let nextSpan = document.querySelector('.mq-cursor + span')
-    // let nextElement = nextVar ? nextVar : nextSpan
-    // console.log('nextElement = ', nextElement)
-
-    const returnCursorParentTree = (child, _output) =>{
-      let output = [child]
-      let parent = child.parentNode
-      output.push(parent)
-
-      let cn = parent.className
-      console.log('cn = ', cn)
-      if (cn == 'mq-root-block' || cn == 'mq-root-block mq-hasCursor') {
-        returnCursorParentTree(parent, output)
+        if (rootNode == parent) {
+          
+          this.setState({
+            latex
+          })
+          return  
+        }
+        returnCursorParentTree(parent, latex)
       }
-    
-      return output
     }
 
-    const iterateUntilTarget = () => {
-
-    }
 
     let child = document.querySelector('.mq-cursor').parentNode
     if (child) {
-      let output = returnCursorParentTree(child)
-      // let cn = output[0].className
-      // console.log('cn = ', cn)
-      // if (cn == 'mq-root-block' || cn == 'mq-root-block mq-hasCursor') {
-      //   console.log('YOU HIT IT!!!')
-      // }
-      
-      console.log('output = ', output)
+      returnCursorParentTree(child)
     }  
   }
   handleKeyDownEvents(e) {
